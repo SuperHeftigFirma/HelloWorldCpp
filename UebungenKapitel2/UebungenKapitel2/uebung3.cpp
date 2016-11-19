4a6bc2cf83890bf749096dfcf9b6f93bf3bb3316
Patrick Lange <patrick.lange@hotmail.de>
2016-11-19 03:16:17 +0100

    Schreibfehler und Optimierung

--- UebungenKapitel2/UebungenKapitel2/uebung3.cpp
+++ UebungenKapitel2/UebungenKapitel2/uebung3.cpp
@@ -5,7 +5,7 @@
 double getHight()
 {
 	double h;
-	std::cout << "Gebe die anfaengliche Höhe in Metern eines sich in Ruhe befindlichen Koerpers an, der dann unter Vernachlaessigung der Luftreibung zu Boden faellt: ";
+	std::cout << "Gebe die anfaengliche Hoehe in Metern eines sich in Ruhe befindlichen Koerpers an, der dann unter Vernachlaessigung der Luftreibung zu Boden faellt: ";
 	std::cin >> h;
 
 	return h;
@@ -29,10 +29,12 @@ void uebung3()
 	const double initialHeight = getHight();
 
 	int i = 0;
+
+	double h;
 	
-	while (calculateNewHight(initialHeight, i) > 0)
+	while ( ( h = calculateNewHight(initialHeight, i) ) > 0)
 	{
-		printHight(calculateNewHight(initialHeight, i), i);
+		printHight(h, i);
 
 		i++;
 	}
